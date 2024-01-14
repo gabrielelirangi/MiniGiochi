90a78c1d5c04f3e4df64aec1d3360845a2c683c9
gabrielelirangi <gabrieleelirangi@gmail.com>
2024-01-14 19:05:51 +0100

    aggiunta logica varia

--- scriptHme.js
+++ scriptHme.js
@@ -2,6 +2,9 @@ let aside = document.getElementById("aside");
 let menu = document.getElementById("menu");
 let boxCard = document.getElementById("box-card");
 
+
+
+
 // function che mostra e toglie il menu
 menu.addEventListener("click", () => {
   aside.classList.toggle("hidden");
@@ -20,7 +23,7 @@ fetch("/Api.json")
       "md:h-[25vh]" , "md:w-max", "xl:h-[25vh]",
       "rounded-lg", "flex", "flex-col", "gap-3",
       "p-6", "m-4", "relative",
-      
+      "hover:border-2", "hover:border-solid", "hover:border-bg-[#110041]", 
       
     );
 
@@ -38,3 +41,87 @@ fetch("/Api.json")
 .catch((error) =>
   console.error("Errore durante il recupero del file JSON:", error)
 );
+
+
+
+// evento che illumina i bordi dei pulsanti social
+let social = document.getElementById("social");
+let social2 = document.getElementById("social2");
+let social3 = document.getElementById("social3");
+let omino = document.getElementById("profilo")
+
+social.addEventListener("mouseover" , () => {
+  social.classList.add("border-2", "border-solid");
+  social.style.borderColor= "#7F3DFF";
+});
+
+social.addEventListener("mouseout" , () => {
+  social.classList.remove("border-2", "border-solid")
+  social.style.borderColor= "";
+});
+
+social2.addEventListener("mouseover" , () => {
+  social2.classList.add("border-2", "border-solid");
+  social2.style.borderColor= "#7F3DFF";
+});
+
+social2.addEventListener("mouseout" , () => {
+  social2.classList.remove("border-2", "border-solid")
+  social2.style.borderColor= "";
+});
+
+social3.addEventListener("mouseover" , () => {
+  social3.classList.add("border-2", "border-solid");
+  social3.style.borderColor= "#7F3DFF";
+});
+
+social3.addEventListener("mouseout" , () => {
+  social3.classList.remove("border-2", "border-solid")
+  social3.style.borderColor= "";
+});
+
+omino.addEventListener("mouseover" , () => {
+  omino.style.borderColor= "#7F3DFF";
+});
+
+omino.addEventListener("mouseout" , () => {
+  omino.style.borderColor= "#f8f8ff";
+});
+
+
+
+
+// logica che fa comparire l'input testuale in versione telefono
+
+let ricerca = document.getElementById("ricerca");
+let boxRicerca = document.getElementById("box-ricerca");
+let boxAperto = false; // Flag per tracciare lo stato dell'elemento di ricerca
+
+
+const barraDiRicerca = () => {
+  if (boxAperto) {
+    boxRicerca.innerHTML = "";
+    boxRicerca.classList.remove("h-[7vh]", "w-[100vw]", "absolute", "top-[10vh]");
+  } else {
+    let input = document.createElement("input");
+    input.type = "text";
+    boxRicerca.appendChild(input);
+    boxRicerca.classList.add("h-[7vh]", "w-[100vw]", "absolute", "top-[10vh]");
+    input.classList.add("h-[5vh]", "w-[70vw]", "ml-[15vw]", "rounded-full", "pl-3", "outline-none");
+    input.focus();
+  }
+  boxAperto = !boxAperto;
+};
+
+ricerca.addEventListener("click", barraDiRicerca);
+
+
+
+// logica di ricerca della barra
+
+// da implementare ancora
+
+
+
+
+
